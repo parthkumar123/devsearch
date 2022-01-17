37c1e94fa381f80ac3dce5573d72b7811f495090
Parthkumar Shiyani <shiyaniparth276@gmail.com>
2022-01-17 06:28:40 +0530

    format

--- projects/models.py
+++ projects/models.py
@@ -2,6 +2,8 @@ from django.db import models
 import uuid
 
 # Create your models here.
+
+
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
@@ -11,33 +13,36 @@ class Project(models.Model):
     vote_total = models.IntegerField(default=0, null=True, blank=True)
     vote_ratio = models.IntegerField(default=0, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-    
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
     # without this the name of project will be objects... so to give them a proper name use this..
     def __str__(self):
         return self.title
-    
+
+
 class Review(models.Model):
     VOTE_TYPE = (
         ('up', 'Up Vote'),
         ('down', 'Down Vote'),
     )
-    #owner = 
+    # owner =
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     body = models.TextField(null=True, blank=True)
     value = models.CharField(max_length=200, choices=VOTE_TYPE)
     created = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-        
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
 
     def _str(self):
         return self.value
 
+
 class Tag(models.Model):
     name = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
 
     def __str__(self):
         return self.name
-    
--- projects/views.py
+++ projects/views.py
@@ -3,27 +3,29 @@ from django.http import HttpResponse
 
 projectsList = [
     {
-        'id':'1',
+        'id': '1',
         'title': 'Ecommerce Website',
         'description': 'Fully function ecommerce website.'
     },
     {
-        'id':'2',
+        'id': '2',
         'title': 'Portfolio Website',
         'description': 'Fully function Portfolio website.'
     },
     {
-        'id':'3',
+        'id': '3',
         'title': 'Social Network',
         'description': 'Fully function Social Network.'
     },
 ]
+
 
 def projects(request):
     page = 'projects'
     number = 46
     context = {'page1': page, 'number': number, 'projects': projectsList}
     return render(request, 'projects/projects.html', context)
+
 
 def project(request, pk):
     projectobj = None
